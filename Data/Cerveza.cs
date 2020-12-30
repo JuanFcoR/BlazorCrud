51de028b16f74f0caa0de71c3b5f82af70ef6a26
JuanFcoR <juan_rosa1@ucne.edu.do>
2020-12-30 15:35:31 -0400

    Add project files.

---
+++ Data/Cerveza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Data
+{
+    public partial class Cerveza
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+    }
+}
